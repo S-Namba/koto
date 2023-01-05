7f3f29e21bdbf5c6f92942e3d8fd0ccb33aea831
S-Namba <68408140+S-Namba@users.noreply.github.com>
2023-01-05 20:08:37 +0900

    Add files via upload

--- koto_function.js
+++ koto_function.js
@@ -1,57 +1,6 @@
-function audio01() {
-    document.getElementById('a_01').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_01').play(); //クリックしたら音を再生
-}
-function audio02() {
-    document.getElementById('a_02').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_02').play(); //クリックしたら音を再生
-}
-function audio03() {
-    document.getElementById('a_03').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_03').play(); //クリックしたら音を再生
-}
-function audio04() {
-    document.getElementById('a_04').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_04').play(); //クリックしたら音を再生
-}
-function audio05() {
-    document.getElementById('a_05').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_05').play(); //クリックしたら音を再生
-}
-function audio06() {
-    document.getElementById('a_06').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_06').play(); //クリックしたら音を再生
-}
-function audio07() {
-    document.getElementById('a_07').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_07').play(); //クリックしたら音を再生
-}
-function audio08() {
-    document.getElementById('a_08').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_08').play(); //クリックしたら音を再生
-}
-function audio09() {
-    document.getElementById('a_09').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_09').play(); //クリックしたら音を再生
-}
-function audio10() {
-    document.getElementById('a_10').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_10').play(); //クリックしたら音を再生
-}
-function audio11() {
-    document.getElementById('a_11').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_11').play(); //クリックしたら音を再生
-}
-function audio12() {
-    document.getElementById('a_12').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_12').play(); //クリックしたら音を再生
-}
-function audio13() {
-    document.getElementById('a_13').currentTime = 0; //連続クリックに対応
-    document.getElementById('a_13').play(); //クリックしたら音を再生
-}
 function play_koto(e){
 var gen=e;
 var sound = new Audio(gen+'.mp3');
+//sound.currentTime = 0;
 sound.play();
 }
