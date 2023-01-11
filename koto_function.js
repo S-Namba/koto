e96bff2b76a7bfdf357e0b2501e1fe6a90803ee5
S-Namba <68408140+S-Namba@users.noreply.github.com>
2023-01-11 20:14:45 +0900

    Add files via upload

--- koto_function.js
+++ koto_function.js
@@ -1,15 +1,13 @@
-var str_bar = [];
 var sound = [];
+var str_bar = [];
 
 for (let i = 1; i < 14; i++){
 
   sound[i] = new Audio(i+'.mp3');
   str_bar[i] = document.getElementById('str'+i);
-//  str_bar[i].addEventListener('mousedown',function(){
-  str_bar[i].addEventListener('touchstart',function(){
+  str_bar[i].addEventListener('mousedown' || 'touchstart',function(){
     sound[i].currentTime = 0;
     sound[i].play();
-//    alert(i+'をクリックダウンした');
-
   });
+
 }
