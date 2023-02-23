15c15ef9438d606e25ae7d6af8a2831dfb7eca38
S-Namba <68408140+S-Namba@users.noreply.github.com>
2023-02-23 16:24:19 +0900

    Add files via upload

--- koto_function.js
+++ koto_function.js
@@ -1,24 +1,17 @@
 var sound = [];
 var str_bar = [];
 
-for (let i = 1; i < 14; i++) {
+for (let i = 1; i < 14; i++){
 
-    sound[i] = new Audio(i + '.mp3');
-    str_bar[i] = document.getElementById('str' + i);
+  sound[i] = new Audio(i+'.mp3');
+  str_bar[i] = document.getElementById('str'+i);
+//  str_bar[i].addEventListener('mousedown',function(){
+//    sound[i].currentTime = 0;
+//    sound[i].play();
+//  });
+  str_bar[i].addEventListener('touchstart',function(){
+    sound[i].currentTime = 0;
+    sound[i].play();
+  });
 
-//    if (window.ontouchstart === null) {
-        str_bar[i].addEventListener('touchstart', function () {
-            sound[i].currentTime = 0;
-            sound[i].play();
-        })
-
-//    } else {
-
-//        str_bar[i].addEventListener('mousedown', function () {
-//            sound[i].currentTime = 0;
-//            sound[i].play();
-//        })
-
-//    }
-;
 }
